68cf637dc01de597b0694b6f674aeed10ff9a459
Radwa Khaled <radwaabouzed@gmail.com>
2025-07-23 20:16:57 +0300

    add Comparisons fun

--- ConsoleApp/Calculation.cs
+++ ConsoleApp/Calculation.cs
@@ -7,12 +7,39 @@ using System.Threading.Tasks;
 namespace ConsoleApp
 {
      class Calculation
-    {
+     {
         public void Addition(int num1 , int num2)
         {
             int sum = num1 + num2;
             Console.WriteLine("Result = " + sum);
         }
+        public void Comparisons(int x, int y) 
+        {
+            if (x > y)
+            {
+                Console.WriteLine($"{x} is greater than {y}");
+            }
+            if (x < y)
+            {
+                Console.WriteLine($"{x} is less than {y}");
+            }
+            if (x >= y)
+            {
+                Console.WriteLine($"{x} is greater than or equal to {y}");
+            }
+            if (x <= y)
+            {
+                Console.WriteLine($"{x} is less than or equal to {y}");
+            }
+            if (x == y)
+            {
+                Console.WriteLine($"{x} is equal to {y}");
+            }
+            if (x != y)
+            {
+                Console.WriteLine($"{x} is not equal to {y}");
+            }
+        }
 
-    }
+     }
 }
