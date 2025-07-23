6feeb4080a6f0ed090952cfbd29cd1d89a4720a8
Radwa Khaled <radwaabouzed@gmail.com>
2025-07-23 20:17:07 +0300

    use Comparisons fun

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,13 +10,12 @@ class Program
             //Console.WriteLine($"Hello {name}");
 
             Calculation calc = new Calculation();
-            
             Console.WriteLine("Enter The First Number");
             int num1 = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter The Secound Number");
             int num2 = int.Parse(Console.ReadLine());
             calc.Addition(num1, num2);
-           
+           calc.Comparisons(num1, num2);
         }
 
     }
