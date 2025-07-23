abfa7a6d4d0053f4ca256eca16bdbb29825b0ed6
Radwa Khaled <radwaabouzed@gmail.com>
2025-07-23 19:59:45 +0300

    add new class

---
+++ ConsoleApp/Calculation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+     class Calculation
+    {
+        public void Addition(int num1 , int num2)
+        {
+            int sum = num1 + num2;
+            Console.WriteLine("Result = " + sum);
+        }
+
+    }
+}
