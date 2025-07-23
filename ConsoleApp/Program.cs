2a491f070dc6e3614c8ec0e6468a989abcd958f4
Radwa Khaled <radwaabouzed@gmail.com>
2025-07-23 21:51:57 +0300

    use Account class

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -9,13 +9,21 @@ class Program
             //string name = "Radwa";
             //Console.WriteLine($"Hello {name}");
 
-            Calculation calc = new Calculation();
-            Console.WriteLine("Enter The First Number");
-            int num1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter The Secound Number");
-            int num2 = int.Parse(Console.ReadLine());
-            calc.Addition(num1, num2);
-           calc.Comparisons(num1, num2);
+           // Calculation calc = new Calculation();
+           // Console.WriteLine("Enter The First Number");
+           // int num1 = int.Parse(Console.ReadLine());
+           // Console.WriteLine("Enter The Secound Number");
+           // int num2 = int.Parse(Console.ReadLine());
+           // calc.Addition(num1, num2);
+           // calc.Comparisons(num1, num2);
+
+            Account account = new Account();
+            Console.WriteLine("Enter Your Name");
+            string name = Console.ReadLine();
+            account.SetName(name);           
+            Console.WriteLine($"Hello {account.GetName()}");
+            
+
         }
 
     }
