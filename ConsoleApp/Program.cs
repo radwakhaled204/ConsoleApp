9608bfe8303157a3abfaa5f2a59d634f6cbb2da7
Radwa Khaled <radwaabouzed@gmail.com>
2025-07-23 19:51:31 +0300

    Addition fun

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
+
 namespace ConsoleApp
 {
 class Program
     {
-        static void Main(string[] args)
+        //static void Main(string[] args)
+        //{
+        //    string name = "Radwa";
+        //    Console.WriteLine($"Hello {name}");
+        //}
+        static void Main()
         {
-            string name = "Radwa";
-            Console.WriteLine($"Hello {name}");
-           
-        }
+            int num1 , num2 , sum;
+            
+            Console.WriteLine("Enter The First Number");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter The Secound Number");
+            num2 = int.Parse(Console.ReadLine());
+            sum = num1 + num2;
+            Console.WriteLine($"Result={sum}");
 
+        }
     }
 
 }
+
+
