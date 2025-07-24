186136358f2ae83bfcf173200b8d28e569f56c68
Radwa Khaled <radwaabouzed@gmail.com>
2025-07-24 19:13:26 +0300

    use student class

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,6 +6,9 @@ class Program
     {
         static void Main(string[] args)
         {
+            Student student = new Student("",0);
+            student.Name = Console.ReadLine();
+
             //string name = "Radwa";
             //Console.WriteLine($"Hello {name}");
 
@@ -17,16 +20,16 @@ class Program
            // calc.Addition(num1, num2);
            // calc.Comparisons(num1, num2);
 
-            Account account = new Account("radwa" , 5);
+            //Account account = new Account("radwa" , 5);
 
-            Console.WriteLine("Enter Your Name");
-            string userName = Console.ReadLine();
-            account.name = userName;
-            Console.WriteLine($"Hello {account.name}");
-            Console.WriteLine("Enter Your Deposite");
-            decimal userDeposite = decimal.Parse( Console.ReadLine() );
-            account.Deposite(userDeposite);
-            Console.WriteLine($"Your Deposite is {account.Balance}");
+            //Console.WriteLine("Enter Your Name");
+            //string userName = Console.ReadLine();
+            //account.name = userName;
+            //Console.WriteLine($"Hello {account.name}");
+            //Console.WriteLine("Enter Your Deposite");
+            //decimal userDeposite = decimal.Parse( Console.ReadLine() );
+            //account.Deposite(userDeposite);
+            //Console.WriteLine($"Your Deposite is {account.Balance}");
 
     
 
