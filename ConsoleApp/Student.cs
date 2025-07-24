c9c2000bbd142deadf8607875eca4b1b6de7b5e1
Radwa Khaled <radwaabouzed@gmail.com>
2025-07-24 19:13:12 +0300

    add student class

---
+++ ConsoleApp/Student.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Student
+    {
+        public string Name { get; set; }
+        private int avarage;
+        public Student(string studentName, int studentAvarge)
+        {
+            Name = studentName;
+            avarage = studentAvarge;
+        }
+
+        public int Avarage 
+        {
+            get
+            {
+                return avarage;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    if (value <= 100)
+                    {
+                        avarage = value;
+                    }
+                }
+            }
+        }
+
+        string LetterGrade
+        {
+            get
+            {
+                string letterGrade = string.Empty;
+                if (avarage >= 90)
+                {
+                    letterGrade = "A";
+                }
+                else if (avarage >= 80)
+                {
+                    letterGrade = "B";
+                }
+                else if (avarage >= 70)
+                {
+                    letterGrade = "C";
+                }
+                else if (avarage >= 60)
+                {
+                    letterGrade =  "D";
+                }
+                else  
+                {
+                    letterGrade = "F";
+                }
+                return letterGrade;
+            }
+
+        }
+    }
+}
