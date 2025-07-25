388072d7787ee453064fa53f700a9745ec6320f3
Radwa Khaled <radwaabouzed@gmail.com>
2025-07-25 17:12:35 +0300

    create product class

---
+++ ConsoleApp/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Product
+    {
+
+        public int product;
+        public int CalProduct()
+        {
+            while(product <= 100 )
+            {
+                product = 3 * product;
+            }
+            return product;
+        }
+
+    }
+}
