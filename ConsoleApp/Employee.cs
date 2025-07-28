d7ef3b562ec522994b59f8c33c86441cb24aae37
Radwa Khaled <radwaabouzed@gmail.com>
2025-07-28 20:32:33 +0300

    create employee class

---
+++ ConsoleApp/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Employee
+    {
+        public string FirstName {  get;}
+        public string LastName { get;}
+        private decimal monthlySalary;
+
+        public Employee(string firstName , string lastName , decimal monthlySalary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            MonthlySalary = monthlySalary;
+        }
+
+        public decimal MonthlySalary
+        {
+            get
+            {
+                return monthlySalary;
+            }
+            set
+            {
+                if (value >= 0M)
+                {
+                    monthlySalary = value;
+                }
+            }
+        }
+        public override string ToString() =>
+$"{FirstName,-10} {LastName,-10} {MonthlySalary,10:C}";
+    }
+}
