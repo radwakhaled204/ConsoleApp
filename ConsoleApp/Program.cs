794b53047db19be552e0a6e5e66714db62a31372
Radwa Khaled <radwaabouzed@gmail.com>
2025-07-28 22:20:51 +0300

    use employee class

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,6 +10,16 @@ class Program
     {
         static void Main(string[] args)
         {
+            
+            var employees = new[] {
+            new Employee("Jason", "Red", 5000M), 
+            new Employee("Ashley", "Green", 7600M), 
+            new Employee("Matthew", "Indigo", 3587.5M),
+            new Employee("James", "Indigo", 4700.77M),
+            new Employee("Luke", "Indigo", 6200M), 
+            new Employee("Jason", "Blue", 3200M), 
+            new Employee("Wendy", "Brown", 4236.4M)};
+
 
 
             int[] array = new[] { 2, 9, 5, 0, 3, 7, 1, 4, 8, 5};
