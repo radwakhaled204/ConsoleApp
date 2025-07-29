9f311d40ffaf397e20b0c4bc6a7525eee52f0fa7
Radwa Khaled <radwaabouzed@gmail.com>
2025-07-29 14:44:11 +0300

    create list

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,50 +10,61 @@ class Program
     {
         static void Main(string[] args)
         {
+            var items = new List<string>();
+            Console.WriteLine("Before adding to items: " +
+                $"Count = {items.Count}; Capacit= {items.Capacity}");
+             items.Add("red");
+             items.Add("blue");
+            items.Insert(0, "yellow");
+            foreach (var item in items)
+            {
+                Console.WriteLine(item);
+            }
             
-           // var employees = new[] {
-           // new Employee("Jason", "Red", 5000M), 
-           // new Employee("Ashley", "Green", 7600M), 
-           // new Employee("Matthew", "Indigo", 3587.5M),
-           // new Employee("James", "Indigo", 4700.77M),
-           // new Employee("Luke", "Indigo", 6200M), 
-           // new Employee("Jason", "Blue", 3200M), 
-           // new Employee("Wendy", "Brown", 4236.4M)};
 
-           // Console.WriteLine("Original array:"); 
-           // foreach (var element in employees)
-           // {
-           //     Console.WriteLine(element);
-           // }
-           // var range = from e in employees where (e.MonthlySalary >= 4000M)&&(e.MonthlySalary <= 6000M) select e;
-           // Console.WriteLine("\nEmployees earning in the range" + $"{4000:C}-{6000:C} per month:"); 
-           // foreach (var element in range)
-           // {
-           //     Console.WriteLine(element);
-           // }
-           // var nameSorted =
-           //         from e in employees orderby e.LastName, e.FirstName select e;
-           // if (nameSorted.Any())
-           //      { Console.WriteLine(nameSorted.First()); }
-           //else
-           // {
-           //     Console.WriteLine("not found");
-           // }
-           //var lastname=
-           // from e in employees select e.LastName;
-           // foreach (var element in lastname.Distinct())
-           // {
-           //     Console.WriteLine(element);
-           // }
-           // var names =
-           // from e in employees select new { e.FirstName, e.LastName };
-          
-           // Console.WriteLine("\nNames only:"); 
-           // foreach (var element in names)
-           // {
-           //     Console.WriteLine(element);
-           // }
-           // Console.WriteLine();
+            // var employees = new[] {
+            // new Employee("Jason", "Red", 5000M), 
+            // new Employee("Ashley", "Green", 7600M), 
+            // new Employee("Matthew", "Indigo", 3587.5M),
+            // new Employee("James", "Indigo", 4700.77M),
+            // new Employee("Luke", "Indigo", 6200M), 
+            // new Employee("Jason", "Blue", 3200M), 
+            // new Employee("Wendy", "Brown", 4236.4M)};
+
+            // Console.WriteLine("Original array:"); 
+            // foreach (var element in employees)
+            // {
+            //     Console.WriteLine(element);
+            // }
+            // var range = from e in employees where (e.MonthlySalary >= 4000M)&&(e.MonthlySalary <= 6000M) select e;
+            // Console.WriteLine("\nEmployees earning in the range" + $"{4000:C}-{6000:C} per month:"); 
+            // foreach (var element in range)
+            // {
+            //     Console.WriteLine(element);
+            // }
+            // var nameSorted =
+            //         from e in employees orderby e.LastName, e.FirstName select e;
+            // if (nameSorted.Any())
+            //      { Console.WriteLine(nameSorted.First()); }
+            //else
+            // {
+            //     Console.WriteLine("not found");
+            // }
+            //var lastname=
+            // from e in employees select e.LastName;
+            // foreach (var element in lastname.Distinct())
+            // {
+            //     Console.WriteLine(element);
+            // }
+            // var names =
+            // from e in employees select new { e.FirstName, e.LastName };
+
+            // Console.WriteLine("\nNames only:"); 
+            // foreach (var element in names)
+            // {
+            //     Console.WriteLine(element);
+            // }
+            // Console.WriteLine();
 
 
 
