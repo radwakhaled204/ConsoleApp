bacb7c32a88ee0d294e3cf4669db64a63a4a0f73
Radwa Khaled <radwaabouzed@gmail.com>
2025-07-31 15:55:10 +0300

    create two properites

--- ConsoleApp/CommissionEmployee.cs
+++ ConsoleApp/CommissionEmployee.cs
@@ -8,5 +8,48 @@ namespace ConsoleApp
 {
     class CommissionEmployee
     {
+        public string FirstName { get; }
+        public string LastName { get; } 
+        public string SocialSecurityNumber {  get; }
+        private decimal grossSales;
+        private decimal commissionRate;
+        public CommissionEmployee(string firstName , string lastName , string socialSecuirtyNumber 
+            decimal grossSales, decimal commissionRate)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            SocialSecurityNumber = socialSecuirtyNumber;
+            GrossSales = grossSales;
+            CommissionRate = commissionRate;
+
+        }
+        public decimal GrossSales
+        {
+            get { return grossSales; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value) , 
+                        value, $"{nameof(GrossSales)} must be > 0 " );
+                }
+                grossSales = value;
+            }
+        }
+        public decimal CommissionRate
+        {
+            get { return commissionRate; }
+            set
+            {
+                if (value <= 0 || value >= 1)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value),
+                        value, $"{nameof(CommissionRate)} must be > 0 and < 1");
+                }
+                commissionRate = value;
+
+            }
+        }
+
     }
 }
