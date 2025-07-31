af26d07f3328e68d565e05f12bc2e616b43e4cc6
Radwa Khaled <radwaabouzed@gmail.com>
2025-07-31 19:35:47 +0300

    create SalariedEmployee class

---
+++ ConsoleApp/SalariedEmployee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class SalariedEmployee : Employee
+    {
+        private decimal weeklySalary;
+        public SalariedEmployee(string firstName, string lastName,
+            string socialSecurityNumber, decimal weeklySalary)
+            : base(firstName, lastName, socialSecurityNumber)
+        {
+            WeeklySalary = weeklySalary; 
+        }
+
+    
+        public decimal WeeklySalary
+        {
+            get
+            {
+                return weeklySalary;
+            }
+            set
+            {
+                if (value < 0) 
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value),
+                        value, $"{nameof(WeeklySalary)} must be >= 0");
+                }
+                weeklySalary = value;
+            }
+        }
+
+        
+        public override decimal Earnings() => WeeklySalary;
+
+        
+        public override string ToString() =>
+            $"salaried employee: {base.ToString()}\n" +
+            $"weekly salary: {WeeklySalary:C}";
+    }
+}
