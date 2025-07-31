272785aafac4173e70dce0cca0214d2249c0f3cc
Radwa Khaled <radwaabouzed@gmail.com>
2025-07-31 19:19:43 +0300

    create employee class

--- ConsoleApp/Employee.cs
+++ ConsoleApp/Employee.cs
@@ -6,34 +6,55 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
-    class Employee
+    public abstract class Employee
     {
-        public string FirstName {  get;}
-        public string LastName { get;}
-        private decimal monthlySalary;
+            public string FirstName { get; }
+            public string LastName { get; }
+            public string SocialSecurityNumber { get; }
 
-        public Employee(string firstName , string lastName , decimal monthlySalary)
-        {
-            FirstName = firstName;
-            LastName = lastName;
-            MonthlySalary = monthlySalary;
+
+            public Employee(string firstName, string lastName,
+                string socialSecurityNumber)
+            {
+                FirstName = firstName;
+                LastName = lastName;
+                SocialSecurityNumber = socialSecurityNumber;
+            }
+
+           
+            public override string ToString() => $"{FirstName} {LastName}\n" +
+                $"social security number: {SocialSecurityNumber}";
+
+          
+            public abstract decimal Earnings(); 
         }
 
-        public decimal MonthlySalary
-        {
-            get
-            {
-                return monthlySalary;
-            }
-            set
-            {
-                if (value >= 0M)
-                {
-                    monthlySalary = value;
-                }
-            }
-        }
-        public override string ToString() =>
-$"{FirstName,-10} {LastName,-10} {MonthlySalary,10:C}";
-    }
+        //        public string FirstName {  get;}
+        //        public string LastName { get;}
+        //        private decimal monthlySalary;
+
+        //        public Employee(string firstName , string lastName , decimal monthlySalary)
+        //        {
+        //            FirstName = firstName;
+        //            LastName = lastName;
+        //            MonthlySalary = monthlySalary;
+        //        }
+
+        //        public decimal MonthlySalary
+        //        {
+        //            get
+        //            {
+        //                return monthlySalary;
+        //            }
+        //            set
+        //            {
+        //                if (value >= 0M)
+        //                {
+        //                    monthlySalary = value;
+        //                }
+        //            }
+        //        }
+        //        public override string ToString() =>
+        //$"{FirstName,-10} {LastName,-10} {MonthlySalary,10:C}";
+    
 }
