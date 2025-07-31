eafb345b5bb05ce6bb9c5614851b5a2e7ee9c326
Radwa Khaled <radwaabouzed@gmail.com>
2025-07-31 19:44:54 +0300

    modify BasePlusCommissionEmployee class

--- ConsoleApp/BasePlusCommissionEmployee.cs
+++ ConsoleApp/BasePlusCommissionEmployee.cs
@@ -8,35 +8,26 @@ namespace ConsoleApp
 {
     class BasePlusCommissionEmployee : CommissionEmployee
     {
-        //public string FirstName { get; }
-        //public string LastName { get; }
-        //public string SocialSecurityNumber { get; }
-        //private decimal grossSales;
-        //private decimal commissionRate;
-        private decimal baseSalary;
-        public BasePlusCommissionEmployee(string firstName, string lastName, string socialSecuirtyNumber,
-            decimal grossSales, decimal commissionRate , decimal baseSalary) :base ( firstName,  lastName,  socialSecuirtyNumber,
-             grossSales,  commissionRate)
-        {
-                    
-            //FirstName = firstName;
-            //LastName = lastName;
-            //SocialSecurityNumber = socialSecuirtyNumber;
-            //GrossSales = grossSales;
-            //CommissionRate = commissionRate;
-            BaseSalary = baseSalary;
 
+
+        private decimal baseSalary; 
+
+        public BasePlusCommissionEmployee(string firstName, string lastName,
+            string socialSecurityNumber, decimal grossSales,
+            decimal commissionRate, decimal baseSalary)
+            : base(firstName, lastName, socialSecurityNumber, grossSales, commissionRate)
+        {
+            BaseSalary = baseSalary;
         }
 
-        //public decimal GrossSales { get; set; }
-        //public decimal CommissionRate { get; set; }
         public decimal BaseSalary
         {
-            get { return baseSalary; }
+            get => baseSalary;
             set
             {
                 if (value < 0)
-                    throw new ArgumentOutOfRangeException(nameof(value), value, $"{nameof(BaseSalary)} must be >= 0");
+                    throw new ArgumentOutOfRangeException(nameof(value),
+                        value, $"{nameof(BaseSalary)} must be >= 0");
                 baseSalary = value;
             }
         }
@@ -44,6 +35,45 @@ namespace ConsoleApp
         public override decimal Earnings() => BaseSalary + base.Earnings();
 
         public override string ToString() =>
-                $"base-salaried {base.ToString()}\nbase salary: {BaseSalary:C}";
+            $"base-salaried {base.ToString()}\nbase salary: {BaseSalary:C}";
+
+
+        ////public string FirstName { get; }
+        ////public string LastName { get; }
+        ////public string SocialSecurityNumber { get; }
+        ////private decimal grossSales;
+        ////private decimal commissionRate;
+        //private decimal baseSalary;
+        //public BasePlusCommissionEmployee(string firstName, string lastName, string socialSecuirtyNumber,
+        //    decimal grossSales, decimal commissionRate , decimal baseSalary) :base ( firstName,  lastName,  socialSecuirtyNumber,
+        //     grossSales,  commissionRate)
+        //{
+                    
+        //    //FirstName = firstName;
+        //    //LastName = lastName;
+        //    //SocialSecurityNumber = socialSecuirtyNumber;
+        //    //GrossSales = grossSales;
+        //    //CommissionRate = commissionRate;
+        //    BaseSalary = baseSalary;
+
+        //}
+
+        ////public decimal GrossSales { get; set; }
+        ////public decimal CommissionRate { get; set; }
+        //public decimal BaseSalary
+        //{
+        //    get { return baseSalary; }
+        //    set
+        //    {
+        //        if (value < 0)
+        //            throw new ArgumentOutOfRangeException(nameof(value), value, $"{nameof(BaseSalary)} must be >= 0");
+        //        baseSalary = value;
+        //    }
+        //}
+
+        //public override decimal Earnings() => BaseSalary + base.Earnings();
+
+        //public override string ToString() =>
+        //        $"base-salaried {base.ToString()}\nbase salary: {BaseSalary:C}";
     }
 }
