637b2b0a408edf76e01e332782c03853eda0459e
Radwa Khaled <radwaabouzed@gmail.com>
2025-08-05 00:27:19 +0300

    new

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -17,14 +17,14 @@ class Program
         static void Main(string[] args)
         {
 
-            int[,] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-            for (int i = 0; i < 3; i++)
-            {
-                int sum = 0;
-                for (int j = 0; j < 3; j++)
-                    sum += matrix[i, j];
-                Console.WriteLine($"Row {i + 1} Sum = {sum}");
-            }
+            //int[,] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    int sum = 0;
+            //    for (int j = 0; j < 3; j++)
+            //        sum += matrix[i, j];
+            //    Console.WriteLine($"Row {i + 1} Sum = {sum}");
+            //}
 
                 //void Change(string s)
                 //{
